27027a8b93809bcfd72b46fdb91ce0ed78df33cc
captivating-mapping-7374 <mxharris1094@yahoo.com>
2020-01-31 22:17:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,13 +15,12 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  last_element = array.pop
-  puts last_element
+  puts array.pop
  
 end
 
 def remove_element_from_start_of_array(array)
-  puts array
+  
  
 end
 
